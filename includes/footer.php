89b4afd599eafff36b3e922bc442bdedde4af793
juansantana.analista <juansantana.analista@gmail.com>
2025-07-18 14:36:26 -0300

    Alteração cores e whatsapp

--- includes/footer.php
+++ includes/footer.php
@@ -573,6 +573,13 @@
     }
     </style>
 
+    <style>
+    :root {
+        --boticario-green: <?php echo $corPrincipal; ?>;
+        --boticario-light-green: <?php echo $corSecundaria; ?>;
+    }
+    </style>
+
     <!-- Scripts -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
     <script src="assets/js/main.js"></script>
