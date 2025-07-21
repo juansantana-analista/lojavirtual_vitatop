ccddfbf33f0276f65e91d7ebab45fcbb7d67ec98
juansantana-analista <juansantana.analista@gmail.com>
2025-07-21 16:06:25 -0300

    NomeLink

--- includes/functions.php
+++ includes/functions.php
@@ -394,10 +394,12 @@ function formatPriceWithDiscount($preco_original, $produto_id) {
 }
 
 function buscarIdLojinhaPorSlug($slug) {
+    // Normaliza o slug para garantir busca sem acento
+    $slug_normalizado = generateSlug($slug);
     $requestData = [
         'class' => 'LojinhaRestService',
         'method' => 'buscarLojaPorNome',
-        'nome_loja' => $slug
+        'nome_loja' => $slug_normalizado
     ];
 
     $ch = curl_init(API_URL);
